7f1627df4c2f35709fe5d97fdae23e4db7b3ac4b
Orkhan Mammadli <orkhanmm@code.edu.az>
2021-02-15 21:53:17 +0400

    initial comment

---
+++ ng-refresher/src/app/persons/persons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+  styleUrls: ['./persons.component.css']
+})
+export class PersonsComponent implements OnInit {
+  @Input() personList:string[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
